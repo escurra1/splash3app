e1944f991a13b735b1afa074eeb9a7639b021fbc
mescurra <harol.josue.gomez.ruiz@everis.com>
2018-07-02 21:30:53 -0500

    secondView

--- splash3app/ViewController.swift
+++ splash3app/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Splash3")
+        print("Other")
     }
 
     override func didReceiveMemoryWarning() {
